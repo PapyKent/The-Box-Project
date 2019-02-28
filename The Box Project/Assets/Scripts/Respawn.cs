ad688f92d5c4cdd1d3d52451e6663a2e9f48d310
MONTES Yannick <montes.yannick@gmail.com>
2019-02-28 12:35:18 +0100

    Respawn script

---
+++ The Box Project/Assets/Scripts/Respawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("Start"))
+        {
+            m_player.transform.position = transform.position;
+        }
+    }
+
+    [SerializeField]
+    private Player m_player = null;
+}
