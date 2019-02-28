70024b2359a82611d2ff1266e39ff8ef847b204a
PapyKent <kentdu38@gmail.com>
2019-02-28 14:47:14 +0100

    moving platform

---
+++ The Box Project/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MovingPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger");
+
+        if (!isMoving)
+        {
+            platformController.enabled = !platformController.enabled;
+            Debug.Log("active platform");
+            isMoving = true;
+        }
+        
+    }
+
+    [SerializeField]
+    private PlatformController platformController;
+
+    private bool isMoving = false;
+}
