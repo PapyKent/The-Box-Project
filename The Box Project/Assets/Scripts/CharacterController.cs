bd99e58543f8d3f18580b19c0bfe3762911a6091
MONTES Yannick <montes.yannick@gmail.com>
2019-03-09 17:17:23 +0100

    Transform controller

---
+++ The Box Project/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    #region Private
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        m_inputs = new Vector2(Input.GetAxis("Horizontal") * m_groundSpeed, 0.0f);
+        HandleSpriteDirection();
+        m_collisions.Reset();
+        m_groundCollider.CheckVerticalCollisions(ref m_collisions);
+        HandleJump();
+        ApplyGravity();
+        m_inputs.y = m_velocity.y * Time.fixedDeltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(m_inputs);
+    }
+
+    private void HandleSpriteDirection()
+    {
+        if(m_inputs.x != 0.0f)
+        {
+            bool flipX = m_inputs.x > 0.0f;
+            m_sprite.flipX = flipX;
+        }
+    }
+
+    private void HandleJump()
+    {
+        m_pressedJump = Input.GetButtonDown("Jump");
+        m_releasedJump = Input.GetButtonUp("Jump");
+
+        if(!m_isJumping && m_pressedJump)
+        {
+            m_isJumping = true;
+            m_velocity.y = m_maxJumpSpeed;
+        }
+        else if(m_isJumping && m_releasedJump)
+        {
+            if(m_velocity.y < m_minJumpSpeed)
+            {
+                m_velocity.y = m_minJumpSpeed;
+            }
+        }
+        else if(m_isJumping && m_collisions.below)
+        {
+            m_isJumping = false;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if(!m_collisions.below)
+        {
+            m_velocity.y += m_gravity * Time.fixedDeltaTime;
+        }
+        else if(!m_isJumping)
+        {
+            m_velocity.y = 0.0f;
+        }
+    }
+
+    [SerializeField]
+    private float m_groundSpeed = 5.0f;
+    [SerializeField]
+    private float m_maxJumpSpeed = 20.0f;
+    [SerializeField]
+    private float m_minJumpSpeed = 10.0f;
+    [SerializeField]
+    private float m_gravity = -9.81f;
+    [SerializeField]
+    private SpriteRenderer m_sprite = null;
+    [SerializeField]
+    private GroundCharacterCollider m_groundCollider = null;
+
+    private Vector2 m_inputs = Vector2.zero;
+    private Vector2 m_velocity = Vector2.zero;
+    private GroundCharacterCollider.CollisionInfo m_collisions;
+    private bool m_isJumping = false;
+    private bool m_pressedJump = false;
+    private bool m_releasedJump = false;
+    #endregion
+}
---
+++ The Box Project/Assets/Scripts/GroundCharacterCollider.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCharacterCollider : MonoBehaviour
+{
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+        }
+    }
+
+    public void CheckVerticalCollisions(ref CollisionInfo collisions)
+    {
+        Vector2 origin = m_groundCollider.bounds.min;
+        for (int i = 0; i < m_verticalRaycastCount + 1; i++)
+        {
+            Debug.DrawRay(origin, (Vector3.up * -1) * m_shootRayDistance, Color.red);
+            RaycastHit2D hit = Physics2D.Raycast(origin, Vector3.up * -1, m_shootRayDistance, m_collisionMask);
+            
+            if(hit.collider != null )
+            {
+                Debug.Log("On a touché au raycast");
+                if(hit.collider.tag == "Ground")
+                {
+                    Debug.Log("On touche le ground");
+                    collisions.below = true;
+                }
+            }
+            origin = new Vector2(origin.x + (m_groundCollider.bounds.size.x / m_verticalRaycastCount), origin.y);
+            if(origin.x > m_groundCollider.bounds.max.x)
+            {
+                origin = m_groundCollider.bounds.max;
+            }
+        }
+    }
+
+    #region Private
+
+    private void Start()
+    {
+        m_horizontalRaycastCount = Mathf.RoundToInt(m_groundCollider.bounds.size.y / m_distanceBetweenRay);
+        m_verticalRaycastCount = Mathf.RoundToInt(m_groundCollider.bounds.size.x / m_distanceBetweenRay);
+    }
+
+    [SerializeField]
+    private float m_distanceBetweenRay = 0.1f;
+    [SerializeField]
+    private float m_shootRayDistance = 0.1f;
+    [SerializeField]
+    private BoxCollider2D m_groundCollider = null;
+    [SerializeField]
+    private LayerMask m_collisionMask;
+
+    private float m_verticalRaycastCount = 0;
+    private float m_horizontalRaycastCount = 0;
+    #endregion
+}
