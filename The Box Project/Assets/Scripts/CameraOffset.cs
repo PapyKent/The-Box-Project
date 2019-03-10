75bfdf6ee2d951ff278cbdcfb46b554c062c7b06
PapyKent <kentdu38@gmail.com>
2019-03-10 19:33:15 +0100

    camera offset

---
+++ The Box Project/Assets/Scripts/CameraOffset.cs
@@ -0,0 +1,25 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOffset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CinemachineTransposer transposer = vcam.GetCinemachineComponent<CinemachineTransposer>();
+        if (transposer != null)
+            transposer.m_FollowOffset.x += 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    #region Private
+    [SerializeField]
+    CinemachineVirtualCamera vcam = null;
+    #endregion
+}
