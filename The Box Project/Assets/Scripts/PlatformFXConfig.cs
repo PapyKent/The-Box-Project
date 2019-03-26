79ce988bb14241a75d19a0dcb6415069d6c125e5
YannickMontes <montes.yannick@gmail.com>
2019-03-26 20:01:53 +0100

    Platform script (taking FX ino account)

---
+++ The Box Project/Assets/Scripts/PlatformFXConfig.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlatformFXConfig")]
+public class PlatformFXConfig : ScriptableObject
+{
+	public float PlatformDissolveSpeed { get { return m_platformDissolveSpeed; } }
+	public float AppearDissolveAmount { get { return m_appearDissolveAmount; } }
+	public float AppearUVScale { get { return m_appearUVScale; } }
+	public float BeginDisappearDissolveAmount { get { return m_beginDisappearDissolveAmount; } }
+	public float EndDisappearDissolveAmount { get { return m_endDisappearDissolveAmount; } }
+	public float DisappearUVScale { get { return m_disappearUVScale; } }
+
+	#region Private
+
+	[SerializeField]
+	private float m_platformDissolveSpeed = 3.0f;
+
+	[Header("Appear FX")]
+	[SerializeField]
+	private float m_appearDissolveAmount = 0.0f;
+	[SerializeField]
+	private float m_appearUVScale = 0.0f;
+
+	[Header("Disappear FX")]
+	[SerializeField]
+	private float m_beginDisappearDissolveAmount = 0.0f;
+	[SerializeField]
+	private float m_endDisappearDissolveAmount = 0.0f;
+	[SerializeField]
+	private float m_disappearUVScale = 0.0f;
+
+	#endregion Private
+}
---
+++ The Box Project/Assets/Scripts/PlatformFXManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class PlatformFXManager : MonoBehaviour
+{
+	public static PlatformFXManager Instance { get; private set; } = null;
+
+	public PlatformFXConfig GetPlatformFXConfig(GridManager.Color color)
+	{
+		switch (color)
+		{
+			case GridManager.Color.RED:
+				return m_redPlatformConfig;
+
+			case GridManager.Color.YELLOW:
+				return m_yellowPlatformConfig;
+
+			case GridManager.Color.BLUE:
+				return m_bluePlatformConfig;
+		}
+		return null;
+	}
+
+	#region Private
+
+	private void Awake()
+	{
+		if (Instance != null)
+		{
+			Debug.LogWarning("PlatformFXManager already instanced. Destroying new one.");
+		}
+		else
+		{
+			Instance = this;
+		}
+	}
+
+	[SerializeField]
+	private PlatformFXConfig m_redPlatformConfig = null;
+	[SerializeField]
+	private PlatformFXConfig m_bluePlatformConfig = null;
+	[SerializeField]
+	private PlatformFXConfig m_yellowPlatformConfig = null;
+
+	#endregion Private
+}
